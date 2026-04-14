b8f4ecb03790ec6aa72bb5ed0cb405d70456111a
Krijn <krijn@ksoeteman.nl>
2026-04-15 01:32:03 +0200

    CI: fix YAML-inspringing via apart fon2415.mk bestand
    - Device-profiel verplaatst naar fon2415.mk (geen heredoc-inspringing meer)
    - Workflow gebruikt 'cat fon2415.mk >> generic.mk' in plaats van heredoc
    - printf gebruikt voor .config i.p.v. heredoc (zelfde reden)

---
+++ fon2415.mk
@@ -0,0 +1,17 @@
+
+# Fon Fonera Hub FON2415 (Gramofon)
+define Device/fon_fon2415
+  SOC := ar9341
+  DEVICE_VENDOR := Fon
+  DEVICE_MODEL := Fonera Hub FON2415
+  DEVICE_VARIANT := (Gramofon)
+  DEVICE_DTS := qca9341_fon_fon2415
+  DEVICE_DTS_DIR := ../dts
+  IMAGE_SIZE := 15744k
+  KERNEL_SIZE := 2048k
+  IMAGES := sysupgrade.bin factory.bin
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | check-size | append-metadata
+  IMAGE/factory.bin    := append-kernel | append-rootfs | pad-rootfs | check-size
+  DEVICE_PACKAGES := kmod-ath9k
+endef
+TARGET_DEVICES += fon_fon2415
